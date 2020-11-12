7177290559190c9b9533435cc635798b47305ca5
kjm <jm.kang@qualson.com>
2020-11-12 20:43:30 +0900

    feat: 숫자 입력
    - 입력값 벨리데이션 체크 테스트 통과

--- src/main/java/baseball/ValidUtils.java
+++ src/main/java/baseball/ValidUtils.java
@@ -4,10 +4,18 @@ import java.util.Objects;
 
 public class ValidUtils {
     public static boolean isNumeric(String string) {
-        return false;
+        try{
+            Integer.parseInt(string);
+            return true;
+        }catch (Exception ex){
+            return false;
+        }
     }
 
     public static boolean is1Or2(String string) {
+        if (Objects.nonNull(string) && (string.equals("1") || string.equals("2"))){
+            return true;
+        }
         return false;
     }
 }
