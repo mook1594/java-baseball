559efd2099c810352cbdb78603f5d960e1c5822c
kjm <jm.kang@qualson.com>
2020-11-12 21:50:04 +0900

    feat: 볼카운트 출력
    - 계산된 볼카운트 코드로 출력한 문장을 만드는 함수 테스트 통과

--- src/main/java/baseball/BallCountPrinter.java
+++ src/main/java/baseball/BallCountPrinter.java
@@ -1,7 +1,18 @@
 package baseball;
 
 public class BallCountPrinter {
+    private final static String EMPTY_STRING = "";
+
     public static String makeText(String code){
-        return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(numberText(code.charAt(0), "스트라이크"));
+        sb.append(numberText(code.charAt(2), "볼"));
+
+        String result = sb.toString().trim();
+        return result.equals(EMPTY_STRING) ? "낫싱" : result;
+    }
+
+    private static String numberText(final char number, final String ballType){
+        return number == '0' ? EMPTY_STRING : number + ballType + " ";
     }
 }
