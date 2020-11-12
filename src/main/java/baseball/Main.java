074c2f341d7a46ee4fe34445b13c113eac09138e
kjm <jm.kang@qualson.com>
2020-11-12 21:22:29 +0900

    feat: 게임 시작/종료/재시작여부
    - 게임 종료 재시작 여부를 입력받아 게임 반복/종료 처리

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -6,13 +6,19 @@ public class Main {
 
     public static void main(String... args){
         System.out.println("Play Ball!!");
+        String endInput;
+        do {
+            String answer = "713";
 
-        InputScanner scanner = new InputConsoleTypeScanner();
-        String ballCount;
-        do{
-            String input = scanner.inputBallNumber3Length();
-            ballCount = BallCountChecker.check(input, "713");
-            System.out.println(ballCount);
-        } while(!ballCount.equals("3S0B0O"));
+            InputScanner scanner = new InputConsoleTypeScanner();
+            String ballCount;
+            do{
+                String input = scanner.inputBallNumber3Length();
+                ballCount = BallCountChecker.check(input, answer);
+                System.out.println(ballCount);
+            } while(!ballCount.equals("3S0B0O"));
+
+            endInput = scanner.inputEndGame();
+        } while (endInput.equals("1"));
     }
 }
