d3d53736eb28333685e67ad1dba56bb4df86e804
kjm <jm.kang@qualson.com>
2020-11-12 21:54:37 +0900

    fix: 게임 시작/종료/재시작여부
    - 게임 종료 문구 추가

--- src/main/java/baseball/InputConsoleTypeScanner.java
+++ src/main/java/baseball/InputConsoleTypeScanner.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class InputConsoleTypeScanner implements InputScanner {
 
     final String INPUT_BALL_NUMBER_LABEL = "숫자를 입력해주세요 : ";
-    final String INPUT_GAME_END_LABEL = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    final String INPUT_GAME_END_LABEL = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    final String INPUT_GAME_RESTART_LABEL = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     final Scanner sc;
 
     public InputConsoleTypeScanner() {
@@ -26,6 +27,7 @@ public class InputConsoleTypeScanner implements InputScanner {
     @Override
     public String inputEndGame() {
         String input;
+        System.out.println(INPUT_GAME_END_LABEL);
         do{
             System.out.println(INPUT_GAME_END_LABEL);
             input = sc.next();
